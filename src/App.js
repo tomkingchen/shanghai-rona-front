1e3b392dd20af8141119786a6e43a96b77cfd7fc
Tom Chen <tomking.chen@gmail.com>
2022-04-11 21:58:12 +1000

    Add example chart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Router } from '@reach/router';
 import Shanghai from './components/shanghai';
+import Example from './components/example';
 
 function App() {
   return (
     <Router>
       <Shanghai path="/" />
+      <Example path="/example" />
     </Router>
   );
 }
