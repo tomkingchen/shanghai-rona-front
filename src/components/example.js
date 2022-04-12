f7e3829702c12c0a55fa36bc8cb67002bc6394cd
Tom Chen <tomking.chen@gmail.com>
2022-04-12 15:32:09 +1000

    Add buttons to allow pick date ranges

--- src/components/example.js
+++ src/components/example.js
@@ -11,28 +11,11 @@ import {
 } from 'recharts';
 
 
-const initialData = [
-  { name: 1, cost: 4.11, impression: 100 },
-  { name: 2, cost: 2.39, impression: 120 },
-  { name: 3, cost: 1.37, impression: 150 },
-  { name: 4, cost: 1.16, impression: 180 },
-  { name: 5, cost: 2.29, impression: 200 },
-  { name: 6, cost: 3, impression: 499 },
-  { name: 7, cost: 0.53, impression: 50 },
-  { name: 8, cost: 2.52, impression: 100 },
-  { name: 9, cost: 1.79, impression: 200 },
-  { name: 10, cost: 2.94, impression: 222 },
-  { name: 11, cost: 4.3, impression: 210 },
-  { name: 12, cost: 4.41, impression: 300 },
-  { name: 13, cost: 2.1, impression: 50 },
-  { name: 14, cost: 8, impression: 190 },
-  { name: 15, cost: 0, impression: 300 },
-  { name: 16, cost: 9, impression: 400 },
-  { name: 17, cost: 3, impression: 200 },
-  { name: 18, cost: 2, impression: 50 },
-  { name: 19, cost: 3, impression: 100 },
-  { name: 20, cost: 7, impression: 100 },
-];
+const initialData = async () => {
+    const resp =  await fetch('https://api.shanghaicovid.xyz/api/shanghai');
+    const shanghaiResp = await resp.json();
+    return shanghaiResp;
+  };
 
 const getAxisYDomain = (from, to, ref, offset) => {
   const refData = initialData.slice(from - 1, to);
@@ -132,12 +115,12 @@ export default class Example extends PureComponent {
             onMouseUp={this.zoom.bind(this)}
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis allowDataOverflow dataKey="name" domain={[left, right]} type="number" />
+            <XAxis allowDataOverflow dataKey="date" domain={[left, right]} type="number" />
             <YAxis allowDataOverflow domain={[bottom, top]} type="number" yAxisId="1" />
             <YAxis orientation="right" allowDataOverflow domain={[bottom2, top2]} type="number" yAxisId="2" />
             <Tooltip />
-            <Line yAxisId="1" type="natural" dataKey="cost" stroke="#8884d8" animationDuration={300} />
-            <Line yAxisId="2" type="natural" dataKey="impression" stroke="#82ca9d" animationDuration={300} />
+            <Line yAxisId="1" type="natural" dataKey="symptomatic" stroke="#8884d8" animationDuration={300} />
+            <Line yAxisId="2" type="natural" dataKey="asymptomatic" stroke="#82ca9d" animationDuration={300} />
 
             {refAreaLeft && refAreaRight ? (
               <ReferenceArea yAxisId="1" x1={refAreaLeft} x2={refAreaRight} strokeOpacity={0.3} />
--- src/components/shanghai.js
+++ src/components/shanghai.js
@@ -10,69 +10,102 @@ import {
 } from "recharts";
 import { format, parseISO } from "date-fns";
 
+
 const Shanghai = () => {
   const [shanghai, setShanghai] = useState([]);
-
+  
+  async function fetchData(){
+    const url = 'https://api.shanghaicovid.xyz/api/shanghai';
+    const resp = await fetch(url);
+    return await resp.json();
+  }
+  
   useEffect(() => {
     const getShanghai = async () => {
-      const resp =  await fetch('https://api.shanghaicovid.xyz/api/shanghai');
-      const shanghaiResp = await resp.json();
-      setShanghai(shanghaiResp);
+      const jsonResp = await fetchData();
+      setShanghai(jsonResp);
     };
 
     getShanghai();
-    }, []);
+  }, []);
+  
+  async function set1month(){
+    const jsonResp = await fetchData();
+    const last1monthData = jsonResp.slice(-30);
+    setShanghai(last1monthData);
+  };
 
-    return (
-      <div>
-        <h1>Shanghai Daily Cases 日增😷</h1>
-        <ResponsiveContainer width="100%" height={400}>
-          <AreaChart data={shanghai} margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}>
-            <Area type="monotone" dataKey="asymptomatic" stackId="1" stroke="#ffc658" fill="#ffc658" />
-            <Area type="monotone" dataKey="symptomatic" stackId="1" stroke="#8884d8" fill="#8884D8" />
-            <XAxis dataKey="date" tickFormatter={(str1) => {
-              if ( str1 === 'auto' || str1 === 0) {
-                return str1;
-              }else{
-                const dateObj = parseISO(str1);
-                return format(dateObj, "MMM d");
-              }
-            }} />
-            <YAxis />
-            <Tooltip />
-            <CartesianGrid strokeDasharray="3 3" opacity={0.5}/>
-          </AreaChart>
-        </ResponsiveContainer>
-        <h1>Shanghai Accumulated Cases 累计确诊😩</h1>
-        <ResponsiveContainer width="100%" height={400}>
-          <AreaChart data={shanghai} margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}>
-            <Area type="monotone" dataKey="accumulative" stackId="1" stroke="#82CA9D" fill="#82CA9D" />
-            <XAxis dataKey="date" tickFormatter={(str2) => {
-              if ( str2 === 'auto' || str2 === 0){
-                return str2;
-              }else{
-                console.log(str2)
-                const dateObj = parseISO(str2);
-                return format(dateObj, "MMM d");
-              }
-            }}/>
-            <YAxis />
-            <Tooltip />
-            <CartesianGrid strokeDasharray="3 3" opacity={0.5}/>
-          </AreaChart>
-        </ResponsiveContainer>
-      </div>
-    );
+  async function set2weeks(){
+    const jsonResp = await fetchData();
+    const last2weeksData = jsonResp.slice(-14);
+    setShanghai(last2weeksData);
+  };
+  
+  async function set1week(){
+    const jsonResp = await fetchData();
+    const last1weekData = jsonResp.slice(-7);
+    setShanghai(last1weekData);
+  };
+
+  return (
+    <div>
+      <h1>Shanghai Daily Cases 日增😷</h1>
+      <button type="button" className="show last 2 weeks" onClick={set1month}>
+        Last Month
+      </button>
+      <button type="button" className="show last 2 weeks" onClick={set2weeks}>
+        Last 2 Weeks
+      </button>
+      <button type="button" className="show last week" onClick={set1week}>
+        Last Week
+      </button>
+      <p></p>
+      <ResponsiveContainer width="100%" height={400}>
+        <AreaChart data={shanghai} margin={{
+          top: 10,
+          right: 30,
+          left: 10,
+          bottom: 0,
+        }}>
+          <Area type="monotone" dataKey="asymptomatic" stackId="1" stroke="#ffc658" fill="#ffc658" />
+          <Area type="monotone" dataKey="symptomatic" stackId="1" stroke="#8884d8" fill="#8884D8" />
+          <XAxis dataKey="date" tickFormatter={(str1) => {
+            if ( str1 === 'auto' || str1 === 0) {
+              return str1;
+            }else{
+              const dateObj = parseISO(str1);
+              return format(dateObj, "MMM d");
+            }
+          }} />
+          <YAxis />
+          <Tooltip />
+          <CartesianGrid strokeDasharray="3 3" opacity={0.5}/>
+        </AreaChart>
+      </ResponsiveContainer>
+      <h1>Shanghai Accumulated Cases 累计确诊😩</h1>
+      <ResponsiveContainer width="100%" height={400}>
+        <AreaChart data={shanghai} margin={{
+          top: 10,
+          right: 30,
+          left: 10,
+          bottom: 0,
+        }}>
+          <Area type="monotone" dataKey="accumulative" stackId="1" stroke="#82CA9D" fill="#82CA9D" />
+          <XAxis dataKey="date" tickFormatter={(str2) => {
+            if ( str2 === 'auto' || str2 === 0){
+              return str2;
+            }else{
+              const dateObj = parseISO(str2);
+              return format(dateObj, "MMM d");
+            }
+          }}/>
+          <YAxis />
+          <Tooltip />
+          <CartesianGrid strokeDasharray="3 3" opacity={0.5}/>
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default Shanghai;
